032ec5e28bc3d6427e645307f5ff90d2449bb440
Kona Vainerere <konavain@gmail.com>
2026-02-18 14:00:56 -0700

    deleted home.css - left it in index.css

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../home.css";
+import "../index.css";
 
 type Instrument = "All" | "Piano" | "Violin" | "Clarinet" | "Other";
 
